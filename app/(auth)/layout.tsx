c89f6f8b38e7527d8b122b97831e67ace8361669
Hrishikesh Bakshi <bakshihrishikesh1107@gmail.com>
2026-03-06 01:33:06 +0530

    feat: add the sign-in and sign-up routes with clerk's components

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AppLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex justify-center pt-40">{children}</div>;
+};
+
+export default AppLayout;
